791ca9594b02b4197a812c06d1620102d6bf53b3
Miguel Angel <miguelangelgarb@gmail.com>
2025-04-26 00:16:38 -0500

    Aplicación Gestor de Tareas

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-import ctypes
-ctypes.CDLL("libX11.so.6").XInitThreads()
-
+import os
+os.environ["LIBXCB_ALLOW_SLOPPY_LOCK"] = "1"
 
 import tkinter as tk
-from tareas import SimpleTask, Recurring, TaskManager
+from tkinter import ttk
+
+from tareas import Recurring, SimpleTask, TaskManager
 from almacenamiento import JSONAlmacenamiento
 from interfaz import TaskApp
 
+
 def main():
+    print("Iniciando aplicación...")
     almacenamiento = JSONAlmacenamiento("tareas.json")
+    print("Archivo de almacenamiento cargado.")
+    
     datos = almacenamiento.cargar_tareas()
+    print(f"Tareas cargadas: {datos}")
+
     manager = TaskManager()
+    print("Gestor de tareas creado.")
 
     for d in datos:
         if "intervalo" in d:
@@ -20,9 +28,12 @@ def main():
         if d.get("completada"):
             tarea.marcar_completada()
         manager.agregar_tarea(tarea)
+    print("Tareas procesadas.")
 
     root = tk.Tk()
+    print("Ventana creada.")
     app = TaskApp(root, almacenamiento, manager)
+    print("App cargada.")
     app.root.mainloop()
 
 if __name__ == "__main__":
